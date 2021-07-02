8cfba37c9d6fd27165f77ef0711a3aedb68b8b06
Yanis48 <doublecraft.official@gmail.com>
2021-07-02 18:10:15 +0200

    traveller key texture

--- src/main/java/com/yanisbft/aroundtheworld/item/ATWItemGroups.java
+++ src/main/java/com/yanisbft/aroundtheworld/item/ATWItemGroups.java
@@ -1,13 +1,14 @@
 package com.yanisbft.aroundtheworld.item;
 
 import com.yanisbft.aroundtheworld.AroundTheWorld;
+import com.yanisbft.aroundtheworld.block.ATWBlocks;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 
 public class ATWItemGroups {
-    public static final ItemGroup MAIN = register("main", ATWItems.TRAVELLER);
+    public static final ItemGroup MAIN = register("main", ATWBlocks.TRAVELLER);
 
     private static ItemGroup register(String name, ItemConvertible icon) {
         return FabricItemGroupBuilder.build(AroundTheWorld.id(name), () -> new ItemStack(icon));
